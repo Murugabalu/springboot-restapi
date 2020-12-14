d4080a2a201b0f3a05e3749408651c10703ca9b4
Murugabalu <Murugabalu.S@cognizant.com-at-994220056469>
2020-12-14 17:23:14 +0000

    Added a sample comment

--- src/main/java/com/learning/restservices/SpringbootRestapiApplication.java
+++ src/main/java/com/learning/restservices/SpringbootRestapiApplication.java
@@ -2,7 +2,7 @@ package com.learning.restservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//Sample comment being added
 @SpringBootApplication
 public class SpringbootRestapiApplication {
 
